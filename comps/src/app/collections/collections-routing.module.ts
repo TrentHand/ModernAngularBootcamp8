8bcc0fe9ce21f55fb34c22384b1a423db21c7420
Trent Hand <trent_hand@onlifehealth.com>
2020-04-29 08:04:03 -0500

    Section 10 complete

--- comps/src/app/collections/collections-routing.module.ts
+++ comps/src/app/collections/collections-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
-const routes: Routes = [{ path: '', component: CollectionsHomeComponent }];
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CompaniesComponent } from './companies/companies.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ comps/src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css'],
+})
+export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Reviews' },
+  ];
+
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch',
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a fantastic dresser',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
