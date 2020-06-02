842ef32abea770f5fd8e0be5463b9f7896c40944
Trent Hand <trent_hand@onlifehealth.com>
2020-06-02 07:38:08 -0500

    finished lecture 223

---
+++ creditcard/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+
+@NgModule({
+  declarations: [AppComponent, CardFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
